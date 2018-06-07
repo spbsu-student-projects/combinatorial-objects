8985edbd070f5d1832a3e9bccee9d4bd0c84343d
NewNeDimas <40007722+NewNeDimas@users.noreply.github.com>
2018-06-07 03:52:51 +0300

    Update and rename left_factors/runner.cpp to dyck_prefix_sum/runner.cpp

--- dyck_prefix_sum/runner.cpp
+++ dyck_prefix_sum/runner.cpp
@@ -1,9 +1,9 @@
 // Author: Olga Badazhkova (olgabadazhkova@mail.ru)
-#include "left_factors.h"
+#include "dyck_prefix_sum.h"
 
 int main ()
 {
-	CombinatorialObject * calculator = new LeftFactors ();
+	CombinatorialObject * calculator = new DyckPrefixSum ();
 	calculator -> interact ();
 	delete calculator;
 	return 0;
