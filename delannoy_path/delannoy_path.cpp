61296820bf4d53f8599e4d00fc2aed5d6d503a67
Areredify <misha-babenko@yandex.ru>
2018-06-07 17:30:04 +0300

    initial_commit

---
+++ delannoy_path/delannoy_path.cpp
@@ -0,0 +1,331 @@
+// Author: Babenko Mikhail (misha-babenko@yandex.ru)
+#include <cassert>
+#include <string>
+#include <algorithm>
+#include "delannoy_path.h"
+
+int DelannoyPath::max_n = 26;
+
+static int64_t safe_add(int64_t a, int64_t b) 
+{
+  if (a > 0 && b > INT64_MAX - a)
+    return INT64_MAX;
+  return a + b;
+}
+
+static int64_t safe_mult(int64_t a, int64_t b)
+{
+  if (a == 0 || b == 0)
+    return 0;
+  if (a > INT64_MAX / b)
+    return INT64_MAX;
+  return a * b;
+}
+
+static int64_t calc_dnk(int n, int k)
+{
+  int64_t c1 = 1, c2 = 1, res = 0, p = 1;
+
+  if (n < 0 || k < 0)
+    return 0;
+
+  for (int i = 0; i <= std::min(n, k); i++)
+  {
+    res = safe_add(res, safe_mult(c1, safe_mult(c2, p)));
+    p = safe_mult(p, 2);
+    c1 = safe_mult(c1, (n - i)) / (i + 1);
+    c2 = safe_mult(c2, (k - i)) / (i + 1);
+  }
+
+  return res;
+}
+
+static vector<vector<int64_t>> precalc_dnk( int n )
+{
+  vector<vector<int64_t>> res(n + 1, vector<int64_t>(n + 1, 0));
+  res[0][0] = 1;
+  
+  for (int i = 0; i <= n; i++)
+    for (int j = 0; j <= n; j++)
+    {
+      if (i == 0 && j == 0)
+        continue;
+      if (i > 0)
+        res[i][j] += res[i - 1][j];
+      if (j > 0)
+        res[i][j] += res[i][j - 1];
+      if (i > 0 && j > 0)
+        res[i][j] += res[i - 1][j - 1];
+    }
+
+  return res;
+}
+
+vector<vector<int64_t>> DelannoyPath::dnk = precalc_dnk(DelannoyPath::max_n);
+
+string DelannoyPath::name()
+{
+  return "DelannoyPath";
+}
+
+int64_t DelannoyPath::total(int n)
+{
+  if (n % 2 == 1)
+    return 0;
+  if (n == 0)
+    return 1;
+  if (n / 2 > max_n || n < 0)
+    return INT64_MAX;
+
+  return dnk[n / 2][n / 2];
+}
+
+static vector<vector<int>> generate_all_nk(int n, int k)
+{
+  if (n == 0 && k == 0)
+    return vector<vector<int>>({ vector<int>() });
+  
+  vector<vector<int>> res;
+  
+  if (n >= 1)
+  {
+    vector<vector<int>> subseq = generate_all_nk(n - 1, k);
+    for (auto & i : subseq)
+    {
+      i.insert(i.begin(), 1);
+      res.push_back(i);
+    }
+  }
+
+  if (k >= 1)
+  {
+    vector<vector<int>> subseq = generate_all_nk(n, k - 1);
+    for (auto & i : subseq)
+    {
+      i.insert(i.begin(), 2);
+      res.push_back(i);
+    }
+  }
+
+  if (n >= 1 && k >= 1)
+  {
+    vector<vector<int>> subseq = generate_all_nk(n - 1, k - 1);
+    for (auto & i : subseq)
+    {
+      i.insert(i.begin(), 3);
+      res.push_back(i);
+    }
+  }
+
+  return res;
+}
+
+vector<vector<int>> DelannoyPath::generate_all(int n)
+{
+  if (n % 2 == 1 || n < 0)
+    return vector<vector<int>>();
+
+  auto res = generate_all_nk(n / 2, n / 2);
+  for (auto & i : res)
+    while ((int)i.size() < n)
+      i.push_back(0);
+
+  return res;
+}
+
+bool DelannoyPath::is_valid(vector<int> const& v)
+{
+  if (v.size() % 2 == 1)
+    return false;
+
+  int n = (int)v.size() / 2;
+  int cx = 0, cy = 0;
+
+  bool is_tail = false;
+  for (int i = 0; i < (int)v.size(); i++)
+  {
+    if (v[i] == 1)
+      cx += 1;
+    else if (v[i] == 2)
+      cy += 1;
+    else if (v[i] == 3)
+      cx += 1, cy += 1;
+    else if (v[i] == 0)
+      is_tail = true;
+    else
+      return false;
+
+    if (is_tail && v[i] != 0)
+      return false;
+  }
+
+  if (cx != n || cy != n)
+    return false;
+
+  return true;
+}
+
+int64_t DelannoyPath::number_by_object(vector<int> const& v)
+{
+  int cx, cy;
+  cx = cy = (int)v.size() / 2;
+
+  int64_t res = 0;
+  for (auto & i : v)
+  {
+    if (i == 1)
+    {
+      cx--;
+    }
+    if (i == 2)
+    {
+      res = safe_add(res, calc_dnk(cx - 1, cy));
+      cy--;
+    }
+    if (i == 3)
+    {
+      res = safe_add(res, safe_add(calc_dnk(cx - 1, cy), calc_dnk(cx, cy - 1)));
+      cx--;
+      cy--;
+    }
+    if (i == 0)
+      break;
+  }
+
+  return res;
+}
+
+vector<int> DelannoyPath::object_by_number(int n, int64_t k)
+{
+  if (n < 0 || n % 2 == 1 || k >= total(n) || k < 0)
+    return vector<int>();
+
+  vector<int> res;
+  int cx = 0, cy = 0;
+
+  while (cx != n / 2 || cy != n / 2)
+  {
+    int64_t d1, d2;
+
+    d1 = calc_dnk(n / 2 - cx - 1, n / 2 - cy);
+    if (k < d1)
+    {
+      res.push_back(1);
+      k -= 0;
+      cx++;
+      continue;
+    }
+    d2 = calc_dnk(n / 2 - cx, n / 2 - cy - 1);
+    if (k < d1 + d2)
+    {
+      res.push_back(2);
+      k -= d1;
+      cy++;
+      continue;
+    }
+    res.push_back(3);
+    k -= d1 + d2;
+    cx++, cy++;
+  }
+
+  for (int i = (int)res.size(); i < n; i++)
+    res.push_back(0);
+
+  return res;
+}
+
+bool DelannoyPath::prev(vector<int>& v)
+{
+  int n = (int)v.size() / 2;
+  int pos = (int)v.size() - 1;
+  int cx = n, cy = n;
+  while (pos >= 0)
+  {
+    if (v[pos] == 0)
+      pos--;
+    else if (v[pos] == 1)
+      pos--, cx--;
+    else if (v[pos] == 2)
+      if (cx == n)
+        pos--, cy--;
+      else
+        break;
+    else
+      break;
+  }
+
+  if (pos < 0)
+  {
+    for (int i = 0; i < n; i++)
+      v[i] = 3, v[i + n] = 0;
+    return false;
+  }
+  else
+  {
+    if (v[pos] == 2)
+      v[pos] = 1, cy--, cx++;
+    else
+      v[pos] = 2, cx--;
+
+    for (int i = pos + 1; i < (int)v.size(); i++)
+      if (cx != n && cy != n)
+        v[i] = 3, cx++, cy++;
+      else if (cy != n)
+        v[i] = 2, cy++;
+      else if (cx != n)
+        v[i] = 1, cx++;
+      else
+        v[i] = 0;
+  }
+  return true;
+}
+
+bool DelannoyPath::next(vector<int>& v)
+{
+  int n = (int)v.size() / 2;
+  int pos = (int)v.size() - 1;
+  int cx = n, cy = n;
+  while (pos >= 0)
+  {
+    if (v[pos] == 0)
+      pos--;
+    else if (v[pos] == 1)
+      if (cy == n)
+        pos--, cx--;
+      else
+        break;
+    else if (v[pos] == 2)
+      if (cx == n)
+        pos--, cy--;
+      else
+        break;
+    else
+      pos--, cx--, cy--;
+  }
+
+  if (pos < 0)
+  {
+    for (int i = 0; i < n; i++)
+      v[i] = 1, v[i + n] = 2;
+    return false;
+  }
+  else
+  {
+    if (v[pos] == 1)
+      v[pos] = 2, cx--, cy++;
+    else
+      v[pos] = 3, cx++;
+    for (int i = pos + 1; i < (int)v.size(); i++)
+      if (cx != n)
+        v[i] = 1, cx++;
+      else if (cy != n)
+        v[i] = 2, cy++;
+      else
+        v[i] = 0;
+  }
+  return true;
+}
+
+DelannoyPath::~DelannoyPath()
+{
+}
---
+++ delannoy_path/delannoy_path.h
@@ -0,0 +1,51 @@
+// Author: Babenko Mikhail (misha-babenko@yandex.ru)
+#ifndef DELANNOY_PATH_H_
+#define DELANNOY_PATH_H_
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+#include "combinatorial_object.h"
+
+class DelannoyPath : public CombinatorialObject
+{
+  // maximum size of grid for which paths fit into INT64
+  static int max_n;
+
+  // number of dellanoy paths from (0, 0) to (n, k) grid
+  static vector<vector<int64_t>> dnk;
+public:
+  /// total number of objects of length n,
+  /// undefined if negative or overflow
+  virtual int64_t total (int n);
+
+  /// lexicographically sorted vector of all objects of length n
+  /// undefined if negative
+  virtual vector <vector <int> > generate_all (int n);
+
+  /// validity of given object
+  virtual bool is_valid (vector <int> const & v);
+
+  /// 0-based lexicographical number of given valid object,
+  /// undefined if overflow
+  virtual int64_t number_by_object (vector <int> const & v);
+
+  /// object of length n with given 0-based lexicographical number,
+  /// undefined if out of bounds
+  virtual vector <int> object_by_number (int n, int64_t k);
+
+  /// transform valid object to lexicographically previous object,
+  /// true if successful, false and undefined state if was first
+  virtual bool prev (vector <int> & v);
+
+  /// transform valid object to lexicographically next object,
+  /// true if successful, false and undefined state if was last
+  virtual bool next (vector <int> & v);
+
+  /// name of combinatorial object
+  virtual string name ();
+
+  /// virtual destructor
+  virtual ~DelannoyPath();
+};
+#endif // DELANNOY_PATH_H_
